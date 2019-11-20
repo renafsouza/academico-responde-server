3dc6ae9f45196602fd53eb96f299e093399b1ce6
renafsouza <rfsouza710@gmail.com>
2019-11-20 14:48:49 -0300

    make connection to database

--- src/app.js
+++ src/app.js
@@ -1,5 +1,17 @@
 var express = require('express')
-var {sequelize, Model, DataTypes} = require('sequelize')
+var Sequelize = require('sequelize')
+const sequelize = new Sequelize('database', 'root', 'paçoca', {
+  host: '34.95.229.143',
+  dialect: "mysql"/* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
+});
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 var app = express()
 var cors = require('cors')
 app.use(cors())
